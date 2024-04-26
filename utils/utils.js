86324146d09510c71b3d9ebca4aea62083899704
pwtansg <pwtan.edu@gmail.com>
2024-04-26 20:43:44 +0800

    added headers for lambda authorizer token

--- utils/utils.js
+++ utils/utils.js
@@ -2,8 +2,11 @@ function resourceResponse(statusCode, body) {
     return {
         statusCode: statusCode,
         headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*' //enable CORS
+            'Access-Control-Allow-Origin': '*', //enable CORS
+            'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,authorizationToken,X-Api-Key,X-Amz-Security-Token', //added headers for lambda authorizer token
+            // 'Access-Control-Allow-Credentials' : true,
+            'Access-Control-Allow-Methods': 'POST,GET,OPTIONS,PUT,DELETE',
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(body)
     }
